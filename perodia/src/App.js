ce4544b291c899031ff3dd91f3bc2e8bc7e43ad6
Austin Duda <aduda0821@gmail.com>
2021-08-26 19:28:12 -0500

    layer panel controls update

--- perodia/src/App.js
+++ perodia/src/App.js
@@ -1,18 +1,50 @@
 import './styles/app.scss';
 import './styles/typography.scss';
+import React, { useState } from 'react';
 import Layers from './components/Layers';
 import Viewport from "./components/Viewport";
-import React, { useState } from 'react';
+
 
 function App() {
 
   const [roomWidth, setRoomWidth] = useState(0);
   const [roomHeight, setRoomHeight] = useState(0);
+  const [canvasOffset, setCanvasOffset] = useState({ x: 0, y: 0 });
+  const [layerData, setLayerData] = useState([
+    {
+        name: 'Layer 1',
+        visible: true,
+        objects: [
+            {
+                type: 'rect',
+                x: 20,
+                y: 20,
+                width: 200,
+                height: 200,
+                fill: 'blue'
+            }
+        ]
+    },
+    {
+      name: 'Layer 2',
+      visible: true,
+      objects: [
+        {
+            type: 'rect',
+            x: 50,
+            y: 50,
+            width: 50,
+            height: 50,
+            fill: 'red'
+        }
+      ]
+    }
+  ])
 
   return (
     <section id="app">
-        <Layers { ...{ setRoomWidth, setRoomHeight, roomWidth, roomHeight} }/>
-        <Viewport/>
+        <Layers { ...{ setRoomWidth, setRoomHeight, roomWidth, roomHeight, layerData, setLayerData,  setCanvasOffset} }/>
+        <Viewport { ...{layerData, canvasOffset, setCanvasOffset}}/>
     </section>
   );
 }
--- perodia/src/components/Layers.jsx
+++ perodia/src/components/Layers.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import '../styles/layers.scss'; 
-import { AXIS } from '../utilities/Enums.js';
+import { AXIS, LAYERPROPS } from '../utilities/Enums.js';
 
-const Layers = ({setRoomWidth, setRoomHeight, roomWidth, roomHeight, ...rest}) => {
+import iconAlign from '../assets/images/icon-align.svg';
+import iconCreate from '../assets/images/icon-create.svg';
+import iconDelete from '../assets/images/icon-delete.svg';
+import iconLayers from '../assets/images/icon-layers.svg';
+import iconVisible from '../assets/images/icon-visible.svg';
 
+const Layers = ({setRoomWidth, setRoomHeight, roomWidth, roomHeight, layerData, setLayerData, canvasOffset, setCanvasOffset, ...rest}) => {
+
+    /* Handles room size updates and input validation */
     const updateRoomSize = (value, axis) => {
         value = +value.replace(/\D/g, "");
         value = value !== '' || !isNaN(value) ? value : 0;
@@ -12,14 +19,63 @@ const Layers = ({setRoomWidth, setRoomHeight, roomWidth, roomHeight, ...rest}) =
         axis ? setRoomHeight(value) : setRoomWidth(value);
     }
 
+    /* Updates layer properties */
+    const updateLayerProperty = (index, property, value) => {
+        setLayerData(
+            layerData.map((item, i) => {
+                return i === index ? {...item, [property] : value} : item 
+            })
+        )
+    }
+    
+    /* Creates new layers */
+    const createNewLayer = () => {
+        setLayerData(oldLayerData => [...oldLayerData, {
+            name: 'Layer ' + (oldLayerData.length + 1),
+            visible: true,
+            objects: [{}]
+        }]);
+    }
+
+    /* Delete layers */
+    const deleteLayer = (index) => {
+        setLayerData(layerData.filter((item, i) => i !== index));
+    }
+
+    const realignCanvas = () => {
+        setCanvasOffset({x: 0, y: 0})
+    }
+
+    /* Return elements */
     return (
         <div>
-            <div >
-                <p className='panel'>Layers</p>
-                <button>+</button>
+            <div className='panel-layers'>
+                <p className='panel-title'>Layers</p>
+                <div className='layers'>
+                    {layerData.map((layer, index) => {
+                        return (
+                            <div key={index} className='layer'>
+                                <img src={iconLayers} width="20" height="20" alt="Layer icon"/>
+                                <input value={layer.name} onChange={(e) => { updateLayerProperty(index, LAYERPROPS.NAME, e.target.value) }}></input>
+                                
+                                <button className="layer-toggle" data-visible={layer.visible} onClick={(e) => { updateLayerProperty(index, LAYERPROPS.VISIBLE, !layer.visible) }}>
+                                    <img src={iconVisible} width="21" height="16" alt="Toggle layer visibility"/>
+                                </button>
+                                
+                                <button className="layer-delete" onClick={(e) => { deleteLayer(index) }}>
+                                    <img src={iconDelete} width="16" height="16" alt="Delete layer"/>
+                                </button>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className='panel-controls'>
+                    <button onClick={createNewLayer}><img src={iconCreate} width="20" height="20" alt="Create new layer"/></button>
+                    <button onClick={realignCanvas}><img src={iconAlign} width="20" height="20" alt="Center canvas offset"/></button>
+                </div>
             </div>
             <div >
-                <p className='panel'>Room Settings</p>
+                <p className='panel-title'>Room Settings</p>
                 <div className='panel-inputs'>
                     <label>
                         <span>Width:</span>
